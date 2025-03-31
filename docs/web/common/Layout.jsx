0aa7ee3ccfff0c9a29325ecffd36bbb2c1db34f4
zxkv <zxkvv@qq.com>
2025-04-01 00:06:26 +0800

    feat(web): 添加前端导航布局和数据展示
    在 `Layout.jsx` 中引入 `link_web` 数据，并实现导航栏、内容区和页脚的布局。同时，更新 `Layout.css` 以支持新的布局样式和交互效果。

--- docs/web/common/Layout.jsx
+++ docs/web/common/Layout.jsx
@@ -1,7 +1,36 @@
+import { link_web } from "../data/index.js";
 import "./Layout.css";
 
 const Layout = () => {
-	return <div className="lay-web">LayHeader</div>;
+	const links = [...link_web, ...link_web, ...link_web, ...link_web];
+
+	return (
+		<div className="lay-web">
+			<div className="lw-header">
+				<div className="lwh-l">
+					<strong>前端导航</strong>
+				</div>
+				<div className="lwh-r">
+					<a href="https://zxk.netlify.app" target="_blank">
+						我的博客
+					</a>
+				</div>
+			</div>
+			<div className="lw-center">
+				{links.map((v, i) => (
+					<a href={v.link} target="_blank">
+						<div className="lwc-item" key={i}>
+							<div className="lwc-title">{v.name}</div>
+							<div className="lwc-desc">{v.desc}</div>
+						</div>
+					</a>
+				))}
+			</div>
+			<div className="lw-footer">
+				<p>©2022-2025 Create By · 下落香樟树</p>
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
---
+++ docs/web/data/index.js
@@ -0,0 +1,29 @@
+export const link_ai = [
+	{
+		name: "",
+		link: "",
+		icon: "",
+		desc: ""
+	}
+];
+
+export const link_web = [
+	{
+		name: "下落香樟树",
+		link: "https://zxkv.onrender.com",
+		icon: "",
+		desc: "一个基于 WebGL 的下落香樟树动画"
+	},
+	{
+		name: "关于",
+		link: "https://zxkv.onrender.com/about",
+		icon: "",
+		desc: "关于我"
+	},
+	{
+		name: "归档",
+		link: "https://zxkv.onrender.com/archives",
+		icon: "",
+		desc: "文章归档"
+	}
+];
