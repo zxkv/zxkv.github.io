cc7d78ed77f0bbf0ee3e37dc39a1062ae4bf441d
zxkv <zxkvv@qq.com>
2025-08-24 21:09:29 +0800

    docs: 添加多个前端相关资源链接到文档
    添加 Nuxt.js、Webpack、Rollup、Turbopack、Docusaurus、Storybook、Dumi 等多个前端框架和工具的链接
    更新 Element-Plus 的图标链接
    新增 CSS 框架、Node.js 生态和代码静态部署平台分类

--- docs/web/data/index.js
+++ docs/web/data/index.js
@@ -44,6 +44,12 @@ export const link_web = [
 				link: "https://pinia.vuejs.org/zh/",
 				icon: "https://pinia.vuejs.org/logo.png",
 				desc: "值得你喜欢的 Vue Store，Vue 官方推荐的状态管理库"
+			},
+			{
+				name: "Nuxt.js",
+				link: "https://nuxt.com/",
+				icon: "https://nuxt.com/icon.png",
+				desc: "Vue.js 的通用应用框架，支持SSR和静态站点生成"
 			}
 		]
 	},
@@ -73,6 +79,24 @@ export const link_web = [
 				link: "https://rspack.dev/zh/",
 				icon: "https://assets.rspack.dev/rspack/favicon-128x128.png",
 				desc: "基于 Rust 的高性能 Web 打包工具"
+			},
+			{
+				name: "Webpack",
+				link: "https://webpack.js.org/",
+				icon: "https://webpack.js.org/icon_180x180.png",
+				desc: "静态模块打包工具，适用于现代 JavaScript 应用程序"
+			},
+			{
+				name: "Rollup",
+				link: "https://rollupjs.org/",
+				icon: "https://rollupjs.org/favicon.png",
+				desc: "JavaScript 模块打包器，适用于库和应用程序"
+			},
+			{
+				name: "Turbopack",
+				link: "https://turbo.build/pack",
+				icon: "https://turbo.build/images/favicon-dark/apple-touch-icon.png",
+				desc: "基于 Rust 的增量打包工具，Webpack 的继任者"
 			}
 		]
 	},
@@ -90,6 +114,24 @@ export const link_web = [
 				link: "https://vitepress.dev/zh/",
 				icon: "https://vitepress.dev/vitepress-logo-large.svg",
 				desc: "VitePress | 由 Vite 和 Vue 驱动的静态站点生成器"
+			},
+			{
+				name: "Docusaurus",
+				link: "https://docusaurus.io/zh-CN",
+				icon: "https://docusaurus.io/img/docusaurus.ico",
+				desc: "基于 React 的静态网站生成器，专注于文档网站"
+			},
+			{
+				name: "Storybook",
+				link: "https://storybook.js.org/",
+				icon: "https://storybook.js.org/icon.svg",
+				desc: "UI 组件开发和文档工具"
+			},
+			{
+				name: "Dumi",
+				link: "https://d.umijs.org/",
+				icon: "https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png",
+				desc: "基于 Umi 的组件/文档研发工具"
 			}
 		]
 	},
@@ -99,7 +141,7 @@ export const link_web = [
 			{
 				name: "Element-Plus",
 				link: "https://cn.element-plus.org/zh-CN/",
-				icon: "https://cn.element-plus.org/apple-touch-icon.png",
+				icon: "https://element.eleme.cn/favicon.ico",
 				desc: "基于 Vue 3，面向设计师和开发者的组件库"
 			},
 			{
@@ -125,6 +167,30 @@ export const link_web = [
 				link: "https://tdesign.tencent.com/vue-next/overview",
 				icon: "https://tdesign.tencent.com/favicon.ico",
 				desc: "TDesign 适配桌面端的组件库，适合在 vue3.x 技术栈项目中使用。"
+			},
+			{
+				name: "Naive UI",
+				link: "https://www.naiveui.com/zh-CN",
+				icon: "https://www.naiveui.com/assets/naivelogo-93278402.svg",
+				desc: "一个 Vue 3 组件库，比较完整，主题可调，使用 TypeScript"
+			},
+			{
+				name: "Vant",
+				link: "https://vant-ui.github.io/vant/",
+				icon: "https://fastly.jsdelivr.net/npm/@vant/assets/logo.png",
+				desc: "轻量、可靠的移动端 Vue 组件库"
+			},
+			{
+				name: "Vuetify",
+				link: "https://vuetifyjs.com/zh-Hans/",
+				icon: "https://vuetifyjs.com/favicon.ico",
+				desc: "Material Design 风格的 Vue UI 组件库"
+			},
+			{
+				name: "PrimeVue",
+				link: "https://primevue.org/",
+				icon: "https://primevue.org/favicon.ico",
+				desc: "功能丰富的开源 Vue UI 组件库"
 			}
 		]
 	},
@@ -148,6 +214,18 @@ export const link_web = [
 				link: "https://gitee.com/",
 				icon: "https://gitee.com/favicon.ico",
 				desc: "国内领先的企业级研发效能和开源代码托管平台"
+			},
+			{
+				name: "GitLab",
+				link: "https://gitlab.com/",
+				icon: "https://gitlab.com/assets/favicon-72a2cad5025aa931d6ea56c3201d1f18e68a8cd39788c7c80d5b2b82aa5143ef.png",
+				desc: "完整的 DevOps 平台，提供代码托管、CI/CD 等功能"
+			},
+			{
+				name: "Bitbucket",
+				link: "https://bitbucket.org/",
+				icon: "https://bitbucket.org/favicon.ico",
+				desc: "Atlassian 旗下的代码托管平台，与 Jira 等工具集成"
 			}
 		]
 	},
@@ -177,6 +255,30 @@ export const link_web = [
 				link: "https://ant.design/index-cn",
 				icon: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
 				desc: "企业级产品设计体系，创造高效愉悦的工作体验"
+			},
+			{
+				name: "Redux",
+				link: "https://cn.redux.js.org/",
+				icon: "https://cn.redux.js.org/img/redux.svg",
+				desc: "JavaScript 应用的状态容器，提供可预测的状态管理"
+			},
+			{
+				name: "MobX",
+				link: "https://zh.mobx.js.org/",
+				icon: "https://zh.mobx.js.org/assets/mobx.png",
+				desc: "简单、可扩展的状态管理库"
+			},
+			{
+				name: "Gatsby",
+				link: "https://www.gatsbyjs.com/",
+				icon: "https://www.gatsbyjs.com/icons/icon-512x512.png",
+				desc: "基于 React 的静态网站生成器，性能优异"
+			},
+			{
+				name: "Material-UI",
+				link: "https://mui.com/zh/",
+				icon: "https://mui.com/static/favicon.ico",
+				desc: "实现 Google Material Design 的 React 组件库"
 			}
 		]
 	},
@@ -206,6 +308,141 @@ export const link_web = [
 				link: "https://stackoverflow.com/",
 				icon: "https://stackoverflow.com/favicon.ico",
 				desc: "全球最大的技术问答社区"
+			},
+			{
+				name: "CodePen",
+				link: "https://codepen.io/",
+				icon: "https://cpwebassets.codepen.io/assets/favicon/favicon-touch-de50acbf5d634ec6791894eba4ba9cf490f709b3d742597c6fc4b734e6492a5a.png",
+				desc: "前端代码在线编辑与分享平台"
+			},
+			{
+				name: "CSS-Tricks",
+				link: "https://css-tricks.com/",
+				icon: "https://css-tricks.com/favicon.ico",
+				desc: "CSS 技巧和教程网站"
+			},
+			{
+				name: "V8.dev",
+				link: "https://v8.dev/",
+				icon: "https://v8.dev/favicon.ico",
+				desc: "V8 JavaScript 引擎官方博客"
+			}
+		]
+	},
+	{
+		category: "CSS 框架",
+		links: [
+			{
+				name: "Tailwind CSS",
+				link: "https://tailwindcss.com/",
+				icon: "https://tailwindcss.com/favicons/apple-touch-icon.png",
+				desc: "功能类优先的 CSS 框架，用于快速构建自定义设计"
+			},
+			{
+				name: "Bootstrap",
+				link: "https://getbootstrap.com/",
+				icon: "https://getbootstrap.com/docs/5.3/assets/img/favicons/apple-touch-icon.png",
+				desc: "全球最流行的 HTML、CSS 和 JS 库，用于开发响应式布局"
+			},
+			{
+				name: "Bulma",
+				link: "https://bulma.io/",
+				icon: "https://bulma.io/favicons/favicon-32x32.png",
+				desc: "基于 Flexbox 的现代 CSS 框架"
+			},
+			{
+				name: "Windi CSS",
+				link: "https://windicss.org/",
+				icon: "https://windicss.org/assets/logo.svg",
+				desc: "下一代实用工具优先的 CSS 框架"
+			},
+			{
+				name: "UnoCSS",
+				link: "https://unocss.dev/",
+				icon: "https://unocss.dev/favicon.svg",
+				desc: "即时按需的原子化 CSS 引擎"
+			}
+		]
+	},
+	{
+		category: "Node.js 生态",
+		links: [
+			{
+				name: "Node.js",
+				link: "https://nodejs.org/zh-cn",
+				icon: "https://nodejs.org/static/images/favicons/favicon.png",
+				desc: "基于 Chrome V8 引擎的 JavaScript 运行时"
+			},
+			{
+				name: "Express",
+				link: "https://expressjs.com/zh-cn/",
+				icon: "https://expressjs.com/images/favicon.png",
+				desc: "快速、unopinionated 的 Node.js Web 框架"
+			},
+			{
+				name: "Koa",
+				link: "https://koajs.com/",
+				icon: "https://koajs.com/public/images/koa-logo.png",
+				desc: "由 Express 团队设计的新一代 Web 框架"
+			},
+			{
+				name: "Nest.js",
+				link: "https://nestjs.com/",
+				icon: "https://nestjs.com/img/logo-small.svg",
+				desc: "用于构建高效、可靠和可扩展的服务器端应用程序的框架"
+			},
+			{
+				name: "Deno",
+				link: "https://deno.land/",
+				icon: "https://deno.land/favicon.ico",
+				desc: "JavaScript 和 TypeScript 的安全运行时"
+			},
+			{
+				name: "Bun",
+				link: "https://bun.sh/",
+				icon: "https://bun.sh/favicon.ico",
+				desc: "快速的 JavaScript 运行时、打包工具和包管理器"
+			}
+		]
+	},
+	{
+		category: "代码静态部署平台",
+		links: [
+			{
+				name: "Vercel",
+				link: "https://vercel.com/",
+				icon: "https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/180x180.png",
+				desc: "为前端团队打造的平台，提供极佳的开发体验"
+			},
+			{
+				name: "Netlify",
+				link: "https://www.netlify.com/",
+				icon: "https://www.netlify.com/v3/static/favicon/apple-touch-icon.png",
+				desc: "用于自动化现代 web 项目的一体化平台"
+			},
+			{
+				name: "GitHub Pages",
+				link: "https://pages.github.com/",
+				icon: "https://pages.github.com/favicon.ico",
+				desc: "GitHub 提供的静态站点托管服务"
+			},
+			{
+				name: "Cloudflare Pages",
+				link: "https://pages.cloudflare.com/",
+				icon: "https://pages.cloudflare.com/favicon.ico",
+				desc: "Cloudflare 的 JAMstack 平台，用于前端开发者协作"
+			},
+			{
+				name: "Render",
+				link: "https://render.com/",
+				icon: "https://render.com/favicon.ico",
+				desc: "统一平台，构建和运行所有应用和网站"
+			},
+			{
+				name: "Surge",
+				link: "https://surge.sh/",
+				icon: "https://surge.sh/favicon.ico",
+				desc: "面向前端开发者的静态网站发布工具"
 			}
 		]
 	}
